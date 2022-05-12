377a17ac5863c6b05137b3abc14316cdd47e3834
Rodrigo Santos de Sousa <101156579+Rosenkat@users.noreply.github.com>
2022-05-12 09:30:00 -0300

    Programa Finalizado para Revisão

---
+++ ManipuladorDeStrings/Teste_Console/InverteTexto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste_Console
+{
+    class InverteTexto
+    {
+        public int Numeros { get; set; }
+
+        public InverteTexto()
+        {
+
+        }
+
+        public InverteTexto(int numero)
+        {
+            Numeros = numero;
+        }
+
+
+    }
+}
